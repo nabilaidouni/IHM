910d28f1cffca6c1dcb25fe9bdae8856fd8fd017
nabilaidouni <nabil_laidouni@hotmail.fr>
2020-02-10 20:56:47 +0100

    mvc created but empty

---
+++ app/src/main/java/fr/tse/fricmanager/controller/CrudDepenseActivity.java
@@ -0,0 +1,16 @@
+package fr.tse.fricmanager.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.tse.fricmanager.R;
+
+public class CrudDepenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crud_depense);
+    }
+}
