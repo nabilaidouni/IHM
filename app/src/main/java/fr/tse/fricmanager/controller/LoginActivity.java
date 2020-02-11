22fa54428a531710791e27eaaf68ff995f5ed315
Etienne <etienne.ittel@gmail.com>
2020-02-11 15:18:02 +0100

    page login

--- app/src/main/java/fr/tse/fricmanager/controller/LoginActivity.java
+++ app/src/main/java/fr/tse/fricmanager/controller/LoginActivity.java
@@ -1,31 +1,38 @@
-package fr.tse.fricmanager;
+package fr.tse.fricmanager.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.EditText;
 import android.view.View;
 import android.content.Intent;
+import fr.tse.fricmanager.R;
+
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         username = findViewById(R.id.editTextLogin);
         password = findViewById(R.id.editTextPassword);
     }
 
     public void login(View view) {
         if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
-            Intent MainActivity = new Intent(fr.tse.fricmanager.MainActivity.this, fr.tse.fricmanager.MainActivity.class);
-            startActivity(MainActivity);
+            Intent CrudGroupActivity = new Intent(LoginActivity.this, CrudGroupActivity.class);
+            startActivity(CrudGroupActivity);
             //correcct password
         } else {
             //wrong password
         }
+    }
+
+    public void register(View view){
+        Intent RegisterActivity = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(RegisterActivity);
     }
 }
